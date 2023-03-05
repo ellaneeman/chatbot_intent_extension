0b8fe712a7d78787685a9c3673e5547675eb4d56
Ella Neeman <ella.neeman@gmail.com>
2023-03-05 16:48:33 +0200

    config file added

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 from bot import PocBot
-from config import ENVIRONMENT_ID, ASSISTANT_API_KEY, DISCOVERY_INSTANCE_ID, DISCOVERY_API_KEY, DISCOVERY_PROJECT_ID
 from flask import Flask
+import os
+import json
 
 app = Flask(__name__)
 
@@ -24,7 +25,13 @@ def delete():
 
 
 if __name__ == '__main__':
-    poc_bot = PocBot(ENVIRONMENT_ID, ASSISTANT_API_KEY, DISCOVERY_INSTANCE_ID, DISCOVERY_API_KEY, DISCOVERY_PROJECT_ID)
+    f = open(os.path.dirname(__file__) + "/config.json")
+    config = json.load(f)
+    poc_bot = PocBot(environment_id=config["environment_id"],
+                     api_key=config["assistant_api_key"],
+                     discovery_instance_id=config["discovery_instance_id"],
+                     discovery_api_key=config["discovery_api_key"],
+                     discovery_project_id=config["discovery_project_id"],
+                     workspace_id=config["workspace_id"])
     session = poc_bot.create_session()
     app.run(port=5000)
-    # session.bot.query_engine("bake cake")
