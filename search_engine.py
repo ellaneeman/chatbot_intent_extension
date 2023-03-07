47fad56c4316b634a7c5f230faaee0d552fb16c2
Ella Neeman <ella.neeman@gmail.com>
2023-03-07 10:50:48 +0200

    fix discovery_service_url, add info over generated intent to the user

--- search_engine.py
+++ search_engine.py
@@ -2,20 +2,21 @@ import json
 from ibm_watson import DiscoveryV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+
 class SearchEngine:
-    def __init__(self, instance_id, api_key, project_id):
+    def __init__(self, instance_id, api_key, project_id, discovery_service_url):
         self.project_id = project_id
         self.authenticator = IAMAuthenticator(api_key)
         self.discovery = DiscoveryV2(
             version='2020-08-30',
             authenticator=self.authenticator
         )
-        self.discovery.set_service_url(
-            'https://api.us-east.discovery.watson.cloud.ibm.com/instances/{}'.format(instance_id))
+        self.discovery.set_service_url(f'{discovery_service_url}/instances/{instance_id}')
 
     def query(self, text, count=1):
         response = self.discovery.query(project_id=self.project_id, natural_language_query=text,
                                         count=count).get_result()
         if response["matching_results"] > 0:
-            return response["results"][0]["document_passages"][0]["passage_text"]
-        return "Could not find relevant information regarding: {}.".format(text)
+            passage_text = response["results"][0]["document_passages"][0]["passage_text"]
+            return f"Found relevant information regarding: <b>{text}</b>.\n\n{passage_text}"
+        return f"Could not find relevant information regarding: <b>{text}</b>.."
